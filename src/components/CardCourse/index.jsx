e5c8e0675f36f7ce6b8ad940cf034993b78e6672
anh29 <95289593+anh29@users.noreply.github.com>
2022-07-10 23:07:32 +0700

    task 1

--- src/components/CardCourse/index.jsx
+++ src/components/CardCourse/index.jsx
@@ -2,12 +2,18 @@ import styles from "./style.module.scss";
 
 function CardCourse() {
   return (
-    <div>
-      <div className={"tw-bg-blue-500 tw-text-white tw-font-bold tw-text-xl"}>
-        abcsd
+      <div className={styles.introduce}>
+        <img  className={styles.img} src="https://shub-storage.sgp1.cdn.digitaloceanspaces.com/classes/JSLPA/1657447270082" alt="" />
+        <div className={styles.til}>
+          <div className={styles.left}>
+            <p className={styles.name}>English for beginners</p>
+            <p className={styles.id}>JSLPA</p>
+          </div>
+          <div>
+            <img className={styles.icon} src="https://www.shareicon.net/data/512x512/2016/06/04/775696_interface_512x512.png" alt="" />
+          </div>
+        </div>
       </div>
-      <div>123</div>
-    </div>
   );
 }
 
