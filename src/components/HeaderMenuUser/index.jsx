2eb2813e79861e6b23f54e7ae9548c408fa8e5e4
tuannguyensn2001 <devpro2001@gmail.com>
2022-07-23 08:15:56 +0700

    update

--- src/components/HeaderMenuUser/index.jsx
+++ src/components/HeaderMenuUser/index.jsx
@@ -1,52 +1,52 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Paper from "@mui/material/Paper";
-import MenuList from "@mui/material/MenuList";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemIcon from "@mui/material/ListItemIcon";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Paper from '@mui/material/Paper';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
 
-import PersonIcon from "@mui/icons-material/Person";
-import LogoutIcon from "@mui/icons-material/Logout";
-import styles from "./styles.module.css";
-import { useMemo } from "react";
-import useAuthStore from "~/store/useAuthStore";
-import { toast } from "react-toastify";
+import PersonIcon from '@mui/icons-material/Person';
+import LogoutIcon from '@mui/icons-material/Logout';
+import styles from './styles.module.css';
+import { useMemo } from 'react';
+import useAuthStore from '~/store/useAuthStore';
+import { toast } from 'react-toastify';
 
-export default function HeaderMenuUser() {
-  const logOut = useAuthStore((state) => state.logOut);
+export default function HeaderMenuUser({ onClick }) {
+    const logOut = useAuthStore((state) => state.logOut);
 
-  const configs = useMemo(() => {
-    return [
-      {
-        text: "Thông tin cá nhân",
-        icon: PersonIcon,
-      },
-      {
-        text: "Đăng xuất",
-        icon: LogoutIcon,
-        onClick: () => {
-          logOut();
-          toast.success("Đăng xuất thành công");
-        },
-      },
-    ];
-  }, []);
+    const configs = useMemo(() => {
+        return [
+            {
+                text: 'Thông tin cá nhân',
+                icon: PersonIcon,
+            },
+            {
+                text: 'Đăng xuất',
+                icon: LogoutIcon,
+                onClick: () => {
+                    logOut();
+                    toast.success('Đăng xuất thành công');
+                },
+            },
+        ];
+    }, []);
 
-  return (
-    <Paper className={styles.menu} sx={{ width: 320, maxWidth: "100%" }}>
-      <MenuList>
-        {configs.map((config, index) => (
-          <MenuItem key={index} {...config}>
-            <ListItemIcon>
-              <Avatar className={styles.user}>
-                <config.icon className={styles.user_icon} />
-              </Avatar>
-            </ListItemIcon>
-            <ListItemText className={styles.text}>{config.text}</ListItemText>
-          </MenuItem>
-        ))}
-      </MenuList>
-    </Paper>
-  );
+    return (
+        <Paper className={styles.menu} sx={{ width: 320, maxWidth: '100%' }}>
+            <MenuList>
+                {configs.map((config, index) => (
+                    <MenuItem onClick={onClick} key={index} {...config}>
+                        <ListItemIcon>
+                            <Avatar className={styles.user}>
+                                <config.icon className={styles.user_icon} />
+                            </Avatar>
+                        </ListItemIcon>
+                        <ListItemText className={styles.text}>{config.text}</ListItemText>
+                    </MenuItem>
+                ))}
+            </MenuList>
+        </Paper>
+    );
 }
--- src/components/HeaderUser/index.jsx
+++ src/components/HeaderUser/index.jsx
@@ -7,24 +7,37 @@ import HeaderMenuUser from '~/components/HeaderMenuUser';
 import styles from './styles.module.css';
 import useAuthStore from '~/store/useAuthStore';
 import avatarDefault from '~/assets/images/avatar_default.png';
+import { useRef } from 'react';
 
 function HeaderUser() {
     const { user, getAvatar } = useAuthStore((state) => state);
 
+    const tippy = useRef(null);
+
+    const handleClickInside = () => {
+        tippy.current?.hide();
+    };
+
+    //number, boolean, string, null, undefined, object
+
     return (
-        <>
-            <Tippy interactive trigger="click" placement="bottom-end" render={() => <HeaderMenuUser />}>
-                <div className={styles.user_wrap}>
-                    <div className={styles.user}>
-                        <Avatar src={getAvatar() || avatarDefault} />
-                        <div className={styles.user_name}>{user?.username || 'username'}</div>
-                        <div className={styles.user_action}>
-                            <ArrowDropDownIcon fontSize="small" className={styles.icon_down} />
-                        </div>
+        <Tippy
+            onMount={(instance) => (tippy.current = instance)}
+            interactive
+            trigger="click"
+            placement="bottom-end"
+            render={() => <HeaderMenuUser onClick={handleClickInside} />}
+        >
+            <div className={styles.user_wrap}>
+                <div className={styles.user}>
+                    <Avatar src={getAvatar() || avatarDefault} />
+                    <div className={styles.user_name}>{user?.username || 'username'}</div>
+                    <div className={styles.user_action}>
+                        <ArrowDropDownIcon fontSize="small" className={styles.icon_down} />
                     </div>
                 </div>
-            </Tippy>
-        </>
+            </div>
+        </Tippy>
     );
 }
 
