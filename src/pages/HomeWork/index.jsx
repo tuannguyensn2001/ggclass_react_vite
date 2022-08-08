538d36ecd11dc16bfb2788805683922328eb4463
vo manh cuong <vomanhcuong06102003@gmail.com>
2022-08-08 09:10:12 +0700

    ok

--- src/pages/HomeWork/index.jsx
+++ src/pages/HomeWork/index.jsx
@@ -2,6 +2,9 @@ import SidebarLeftHomeWork from '~/components/SidebarLeftHomeWork';
 import HomeWorkContent from '~/components/HomeWorkContent';
 import SiderbarRightHomeWork from '~/components/SiderbarRightHomeWork';
 import LessonHeader from '~/components/LessonHeader';
+import { Worker } from '@react-pdf-viewer/core';
+import { Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 import styles from './styles.module.css';
 const defaultData = [
@@ -15,12 +18,16 @@ const defaultData = [
 function HomeWork() {
     return (
         <div className={styles.wrap}>
-            <LessonHeader name="Bài tập" />
+            {/* <LessonHeader name="Bài tập" />
             <div className={styles.content}>
                 <SidebarLeftHomeWork />
                 <HomeWorkContent data={defaultData} />
                 <SiderbarRightHomeWork />
-            </div>
+            </div> */}
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.14.305/build/pdf.worker.min.js">
+                <Viewer fileUrl="http://truongthptductho.edu.vn/upload/48852/20190504/Vi_tich_phan_va_cam_ung_dien_tu.pdf" />
+                ;
+            </Worker>
         </div>
     );
 }
