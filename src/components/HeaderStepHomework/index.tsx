ee814c8bf3669fab8a46ee700b2d9d902d8c0f4e
vo manh cuong <vomanhcuong06102003@gmail.com>
2022-08-09 16:22:50 +0700

    css home work add

--- src/components/HeaderStepHomework/index.tsx
+++ src/components/HeaderStepHomework/index.tsx
@@ -1,16 +1,17 @@
 import HomeworkStepItem from '~/components/HomeworkStepItem';
 import { memo } from 'react';
-
 interface Prop {
     step: number;
 }
-
+import styles from './styles.module.css';
 function HeaderStepHomework({ step }: Prop) {
     return (
-        <div className={'tw-grid tw-grid-cols-12'}>
-            <HomeworkStepItem active={step === 1} label={'1. Dap an'} />
-            <HomeworkStepItem active={step === 2} label={'2. Mo rong'} />
-            <HomeworkStepItem active={step === 3} label={'3. Thong tin bai tap'} />
+        <div className={styles.wrap}>
+            <div className={'tw-grid tw-grid-cols-12'}>
+                <HomeworkStepItem active={step === 1} label={'1. Đáp án'} />
+                <HomeworkStepItem active={step === 2} label={'2. Mở rộng'} />
+                <HomeworkStepItem active={step === 3} label={'3. Thông tin bài tập'} />
+            </div>
         </div>
     );
 }
